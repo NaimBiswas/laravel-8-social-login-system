d7fbc3ad57db2bd84d4758d3ab12ebe29957eb81
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-07 16:00:23 +0600

    fix a spelling misteck

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redicrect' => env('GITHUB_REDIRECT_URL'),
+        'redirect' => env('GITHUB_REDIRECT_URL'),
     ],
 
 ];
