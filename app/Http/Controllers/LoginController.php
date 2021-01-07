e546dd4c6b3444d4f8252173ea1b9a55add53630
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-07 16:09:05 +0600

    Set all data from github to our applicate using githubRedirect function

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -13,5 +17,15 @@ class LoginController extends Controller
     }
     public function githubRedirect()
     {
+        $user = Socialite::driver('github')->user();
+        $users = User::firstOrCreate([
+            'email' => $user->email
+
+        ], [
+            'name' => $user->name,
+            'password' => Hash::make(Str::random(15)),
+        ]);
+        Auth::login($user, true);
+        return redirect('/dashboard');
     }
 }
